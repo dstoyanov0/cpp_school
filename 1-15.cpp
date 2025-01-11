906bb4ef2edbdd30cddce678b96119a4933370bb
Daniel Stoyanov <d.stoyanov2017@gmail.com>
2025-01-11 14:27:12 +0200

    commit

---
+++ 1-15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double a, b, c, d, h, p, s;
+
+    cin >> a >> b >> c >> d >> h;
+
+    p = a + b + c + d;
+    s = ((a+c)*h)/2;
+
+    cout << "Perimeteryt e: "<< p << endl << "Liceto e: " << s << endl;
+}
+
+// Dimitar Stoyanov is my teacher
